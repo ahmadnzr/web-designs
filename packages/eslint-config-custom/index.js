ae7bb4d4ce7a328a90e9e23bfb81220619c15241
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-25 15:50:51 +0700

    setup vite project

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "turbo",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
