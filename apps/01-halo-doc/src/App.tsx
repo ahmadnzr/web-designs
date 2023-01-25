6af7efdaed9c05efe2e9c615091e90421ae2c93d
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-25 17:02:47 +0700

    chore:setup for tailwindcss

--- apps/01-halo-doc/src/App.tsx
+++ apps/01-halo-doc/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>Halo REact</div>;
+  return (
+    <h1 className="bg-clip-text text-transparent text-lg font-extrabold text-[100px] leading-[100px] bg-gradient-to-tr from-pink-500 to-violet-500">
+      Halo REact
+    </h1>
+  );
 }
 
 export default App;
