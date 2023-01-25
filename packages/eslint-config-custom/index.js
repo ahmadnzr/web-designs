7d98767ab87910dd96c21e26f97e68db881a7ab2
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-25 23:13:27 +0700

    config: update eslint rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -19,9 +19,7 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    "no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
-    ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
   },
 };
