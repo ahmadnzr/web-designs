0b636ce8161bca286c3bcb5fa08359ccc6b82658
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-26 17:09:52 +0700

    [FIX] hidden dialog

--- apps/01-hola-dok/src/components/Dialog.tsx
+++ apps/01-hola-dok/src/components/Dialog.tsx
@@ -5,7 +5,7 @@ import quote from "../assets/quote.png";
 
 const Dialog = () => {
   return (
-    <div className="hidden md:static absolute top-[70%] right-[50%] lg:bottom-initial xl:top-[218px] lg:top-96 xl:right-[485px] lg:right-72 w-[292px] p-[20px] rounded-[10px] shadow-[0_15px_30px_0_rgba(0,0,0,0.1)] bg-white">
+    <div className="hidden md:block absolute top-[70%] right-[50%] lg:bottom-initial xl:top-[218px] lg:top-96 xl:right-[485px] lg:right-72 w-[292px] p-[20px] rounded-[10px] shadow-[0_15px_30px_0_rgba(0,0,0,0.1)] bg-white">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
           <img src={photo} alt="" />
