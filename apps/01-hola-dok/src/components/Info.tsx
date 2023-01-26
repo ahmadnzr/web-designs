d1f2d7fad331a9ae49c1d9bfa1e8579e03fc8f11
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-26 08:53:20 +0700

    [FIX] Responsive Design

--- apps/01-hola-dok/src/components/Info.tsx
+++ apps/01-hola-dok/src/components/Info.tsx
@@ -6,38 +6,50 @@ import cam from "../assets/cam.png";
 
 const Info = () => {
   return (
-    <div className="absolute bottom-[32px] p-[40px] bg-primary text-white font-dmSans flex items-center justify-between gap-[32px]">
-      <div className="flex-1 flex gap-[25px]">
-        <img src={cal} alt="" className="w-[40px] h-[40px]" />
-        <div className="w-[294px]">
-          <h1 className="font-bold text-[18px] leading-[23px]">
+    <div className="hidden absolute bottom-[32px] xl:p-[40px] lg:p-5 bg-primary text-white font-dmSans lg:flex items-center justify-between xl:gap-[32px] lg:gap-2">
+      <div className="flex-1 flex xl:gap-[25px] lg:gap-3">
+        <img
+          src={cal}
+          alt=""
+          className="xl:w-[40px] lg:w-6 xl:h-[40px] lg:h-6"
+        />
+        <div className="xl:w-[294px]">
+          <h1 className="font-bold xl:text-[18px] lg:text-[16px] xl:leading-[23px]">
             Online appointment scheduling
           </h1>
-          <p className="mt-[10px] font-normal text-[16px] leading-[26px] text-white/80">
+          <p className="xl:mt-[10px] lg:mt-[5px] font-normal xl:text-[16px] lg:text-[14px] xl:leading-[26px] text-white/80">
             Allows users to schedule appointments with their healthcare provider
             through the app.
           </p>
         </div>
       </div>
-      <div className="flex-1 flex gap-[25px]">
-        <img src={lonceng} alt="" className="w-[40px] h-[40px]" />
-        <div className="w-[294px]">
-          <h1 className="font-bold text-[18px] leading-[23px]">
+      <div className="flex-1 flex xl:gap-[25px] lg:gap-3">
+        <img
+          src={lonceng}
+          alt=""
+          className="xl:w-[40px] lg:w-6 xl:h-[40px] lg:h-6"
+        />
+        <div className="xl:w-[294px]">
+          <h1 className="font-bold xl:text-[18px] lg:text-[16px] xl:leading-[23px]">
             Medication reminders
           </h1>
-          <p className="mt-[10px] font-normal text-[16px] leading-[26px] text-white/80">
+          <p className="xl:mt-[10px] lg:mt-[5px] font-normal xl:text-[16px] lg:text-[14px] xl:leading-[26px] text-white/80">
             Allows users to schedule appointments with their healthcare provider
             through the app.
           </p>
         </div>
       </div>
-      <div className="flex-1 flex gap-[25px]">
-        <img src={cam} alt="" className="w-[40px] h-[40px]" />
-        <div className="w-[294px]">
-          <h1 className="font-bold text-[18px] leading-[23px]">
+      <div className="flex-1 flex xl:gap-[25px] lg:gap-3">
+        <img
+          src={cam}
+          alt=""
+          className="xl:w-[40px] lg:w-6 xl:h-[40px] lg:h-6"
+        />
+        <div className="xl:w-[294px]">
+          <h1 className="font-bold xl:text-[18px] lg:text-[16px] xl:leading-[23px]">
             Virtual consultations
           </h1>
-          <p className="mt-[10px] font-normal text-[16px] leading-[26px] text-white/80">
+          <p className="xl:mt-[10px] lg:mt-[5px] font-normal xl:text-[16px] lg:text-[14px] xl:leading-[26px] text-white/80">
             Allows users to schedule appointments with their healthcare provider
             through the app.
           </p>
--- apps/01-hola-dok/src/components/Navbar.tsx
+++ apps/01-hola-dok/src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
 import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between relative z-50">
-      <div className="flex items-center w-[199px]">
+      <div className="flex items-center xl:w-[199px] lg:w-44">
         <img src={logo} alt="" />
         <span className="ml-[15px] font-bold text-[18px]">Holadok</span>
       </div>
-      <div className="flex-1 flex items-center gap-[50px] text-[16px] text-black/80">
+      <div className="hidden flex-1 lg:flex items-center gap-[50px] text-[16px] text-black/80">
         <a href="#home" className="font-semibold text-textDark">
           Home
         </a>
@@ -16,11 +18,14 @@ const Navbar = () => {
         <a href="#blog">Blog</a>
         <a href="#abous-us">About Us</a>
       </div>
-      <div className="w-[295px] font-dmSans text-[16px] font-bold leading-[21px] flex justify-end">
+      <div className="hidden flex-1 lg:w-[295px] font-dmSans text-[16px] font-bold leading-[21px] md:flex justify-center lg:justify-end">
         <button className="px-[20px] py-[8px] ">Login</button>
         <button className="px-[20px] py-[8px] border border-[#101C45] ml-[15px]">
           Book Now
         </button>
+      </div>
+      <div className="lg:hidden ml-4">
+        <Bars3Icon className="h-9 w-9 text-textDark" />
       </div>
     </nav>
   );
