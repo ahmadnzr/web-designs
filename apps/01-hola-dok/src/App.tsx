c39f1dee002835483e10a6410fb0da08695bb406
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-01-26 08:56:35 +0700

    [FIX] Responsive Design (#2)

--- apps/01-hola-dok/src/App.tsx
+++ apps/01-hola-dok/src/App.tsx
@@ -10,12 +10,16 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="lg:max-w-[1440px] min-h-[1088px] lg:px-[150px] lg:pt-[58px] mx-auto text-textDark relative">
+    <div className="container pt-3 px-5 xl:max-w-[1440px] min-h-screen xl:min-h-[1088px] lg:min-h-[1010px] xl:px-[150px] lg:px-14 xl:pt-[58px] lg:pt-5 mx-auto text-textDark relative overflow-x-hidden duration-300">
       <Navbar />
       <Hero />
-      <div className="absolute top-[18px] right-[86px] z-0">
-        <img src={bg} alt="" />
-        <img src={docter} alt="" className="absolute bottom-0 left-[56px]" />
+      <div className="absolute min-h-screen lg:min-h-[847px] xl:top-[18px] top-0 -right-20 xl:right-[86px] lg:-right-20 z-0">
+        <img src={bg} alt="" className="h-screen lg:h-[847px]" />
+        <img
+          src={docter}
+          alt=""
+          className="absolute bottom-0 xl:left-[56px] lg:left-9 lg:h-[750px] right-10 h-[600px]"
+        />
       </div>
       <Dialog />
       <Info />
--- apps/01-hola-dok/src/components/Hero.tsx
+++ apps/01-hola-dok/src/components/Hero.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="mt-[151px] w-[457px] font-dmSans">
-      <span className="font-medium text-[48px] leading-[62px]">
+    <div className="absolute lg:static mt-[151px] lg:w-[457px] md:w-[50%] w-[80%] font-dmSans z-10">
+      <span className="font-medium text-2xl xl:text-[48px] lg:text-[32px] xl:leading-[62px]">
         Get the healthcare you need, when you need it.
       </span>
-      <p className="mt-[15px] font-normal text-[18px] leading-[30px]">
+      <p className="xl:mt-[15px] mt-2 font-normal text-sm lg:text-[18px] lg:leading-[30px]">
         Manage all of your healthcare needs in one convenient place – from
         booking appointments to tracking your health history.
       </p>
-      <button className="mt-[50px] px-[35px] py-[15px] bg-primary text-white text-[18px] leading-[30px] font-normal">
+      <button className="xl:mt-[50px] mt-8 xl:px-[35px] lg:px-6 px-4 xl:py-[15px] lg:py-3 py-2 bg-primary text-white text-sm lg:text-[18px] lg:leading-[30px] font-normal">
         Book an Appointment
       </button>
     </div>
