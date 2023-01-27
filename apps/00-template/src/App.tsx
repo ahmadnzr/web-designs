c19f2fa074f75dff3ae251fabb14317f6e195ebc
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-27 07:09:21 +0700

    feat: slicing template

---
+++ apps/00-template/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Bars3Icon } from "icons";
+
+function App() {
+  return (
+    <div>
+      <Bars3Icon className="h-10 w-10 text-red-400" />
+    </div>
+  );
+}
+
+export default App;
