2681ff6ac8f3612f8f63295373986c3081a6be83
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-07-11 21:59:24 +0700

    14: discover page

--- apps/14-active/src/App.tsx
+++ apps/14-active/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   ArrowLeftOnRectangleIcon,
+  ArrowRightIcon,
   ArrowTrendingUpIcon,
   BookmarkSquareIcon,
   BuildingOfficeIcon,
@@ -11,33 +12,174 @@ import {
   InboxIcon,
   ListBulletIcon,
   PaperAirplaneIcon,
+  PlayCircleIcon,
+  PlusIcon,
 } from "icons";
 
 import Heading from "./components/Atoms/Heading";
 import Text from "./components/Atoms/Text";
 import Button from "./components/Atoms/Button";
+import TextInput from "./components/Atoms/TextInput";
+import Title from "./components/Atoms/Title";
+import Box from "./components/Atoms/Box";
 
 function App() {
   return (
     <div className="drawer lg:drawer-open bg-neutral-20">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
+      <div className="drawer-content flex flex-col py-6">
         <div className="w-full navbar">
-          <div className="flex-1 px-2 mx-2">Navbar Title</div>
-          <div className="flex-none hidden lg:block">
-            <ul className="menu menu-horizontal">
-              {/* Navbar menu content here */}``
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
-              <li>
-                <a>Navbar Item 2</a>
-              </li>
-            </ul>
+          <div className="flex-1 px-2 mx-2 flex items-center gap-3">
+            <HomeIcon className="h-[48px] w-[48px]" />
+            <Heading weight="semibold" type="h3">
+              Discover
+            </Heading>
+          </div>
+          <div className="flex-none flex gap-3">
+            <TextInput />
+            <Button icon={<PlusIcon />}>Post a Job</Button>
           </div>
         </div>
         {/* Page content here */}
-        <div className="my-9 mx-5">Content</div>
+        <div className="my-9 mx-10">
+          <div className="grid grid-cols-3 gap-10">
+            <Box className="col-span-2 row-span-3">
+              <Title size="large">Find Talent Your Away</Title>
+              <Text color="text-neutral-40">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Temporibus ipsa, hic voluptatibus sint dolorem, id repellat sit
+                consectetur cumque quaerat dignissimos dolores asperiores
+                inventore commodi maxime nobis fugiat. Est, id!
+              </Text>
+              <div className="flex gap-5">
+                <Button icon={<PlusIcon />}>Post a Job</Button>
+
+                <Button type="ghost" icon={<PlayCircleIcon />}>
+                  Watch Video
+                </Button>
+              </div>
+            </Box>
+            <Box>
+              <Heading type="h3" weight="semibold">
+                Today Activity
+              </Heading>
+              <div>
+                <Heading type="h5" weight="semibold">
+                  124 min
+                </Heading>
+                <Text>60% discover talent</Text>
+              </div>
+            </Box>
+            <Box className="row-span-3">
+              <Heading type="h3" weight="semibold">
+                Top Rate Talents
+              </Heading>
+              <ul className="menu menu-lg p-0 m-0 w-full rounded-box">
+                <li>
+                  <div className="flex items-center gap-4 justify-start">
+                    <img
+                      src="avatar.jpg"
+                      alt=""
+                      className="w-[60px] h-[60px] object-cover rounded-full"
+                    />
+                    <div className="flex flex-col">
+                      <Text
+                        weight="semibold"
+                        type="span"
+                        className=""
+                        size="lg"
+                      >
+                        Nizar Dev
+                      </Text>
+                      <Text type="span" className="" size="sm">
+                        Data Scientist
+                      </Text>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center gap-4 justify-start">
+                    <img
+                      src="avatar.jpg"
+                      alt=""
+                      className="w-[60px] h-[60px] object-cover rounded-full"
+                    />
+                    <div className="flex flex-col">
+                      <Text
+                        weight="semibold"
+                        type="span"
+                        className=""
+                        size="lg"
+                      >
+                        Nizar Dev
+                      </Text>
+                      <Text type="span" className="" size="sm">
+                        Data Scientist
+                      </Text>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center gap-4 justify-start">
+                    <img
+                      src="avatar.jpg"
+                      alt=""
+                      className="w-[60px] h-[60px] object-cover rounded-full"
+                    />
+                    <div className="flex flex-col">
+                      <Text
+                        weight="semibold"
+                        type="span"
+                        className=""
+                        size="lg"
+                      >
+                        Nizar Dev
+                      </Text>
+                      <Text type="span" className="" size="sm">
+                        Data Scientist
+                      </Text>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <Button type="ghost" icon={<ArrowRightIcon />} rightIcon>
+                Project Catalogue
+              </Button>
+            </Box>
+            <Box>
+              <Heading type="h3" weight="semibold">
+                Jalan
+              </Heading>
+              <Button type="ghost" icon={<ArrowRightIcon />} rightIcon>
+                Project Catalogue
+              </Button>
+            </Box>
+            <Box>
+              <Heading type="h3" weight="semibold">
+                Mandi
+              </Heading>
+              <Button type="ghost" icon={<ArrowRightIcon />} rightIcon>
+                Project Catalogue
+              </Button>
+            </Box>
+            <Box className="col-span-2">
+              <Heading type="h3" weight="semibold">
+                Minum
+              </Heading>
+              <Button type="ghost" icon={<ArrowRightIcon />} rightIcon>
+                Project Catalogue
+              </Button>
+            </Box>
+            <Box>
+              <Heading type="h3" weight="semibold">
+                Makan
+              </Heading>
+              <Button type="ghost" icon={<ArrowRightIcon />} rightIcon>
+                Project Catalogue
+              </Button>
+            </Box>
+          </div>
+        </div>
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
