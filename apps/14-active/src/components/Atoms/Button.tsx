720a93bfe44f4edbfd4a11c91f76c73a20c44b93
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-07-11 16:45:42 +0700

    14: update button props

--- apps/14-active/src/components/Atoms/Button.tsx
+++ apps/14-active/src/components/Atoms/Button.tsx
@@ -2,13 +2,14 @@ import clsx from "clsx";
 import React from "react";
 
 interface Props {
-  children: React.ReactNode;
-  type?: "primary" | "secondary" | "ghost";
+  children?: React.ReactNode;
+  type?: "primary" | "secondary" | "ghost" | "neutral" | "error";
   size?: "sm" | "md" | "lg";
   icon?: React.ReactNode;
   rightIcon?: boolean;
   isLoading?: boolean;
   disabled?: boolean;
+  outlined?: boolean;
 }
 const Button = ({
   children,
@@ -18,11 +19,14 @@ const Button = ({
   rightIcon = false,
   isLoading = false,
   disabled = false,
+  outlined = false,
 }: Props) => {
   const buttonType = {
     primary: "btn-primary",
     secondary: "btn-secondary",
     ghost: "btn-ghost",
+    neutral: "btn-neutral-10",
+    error: "btn-error",
   };
 
   const buttonSize = {
@@ -32,7 +36,14 @@ const Button = ({
   };
 
   return (
-    <button className={clsx("btn", buttonType[type], buttonSize[size])}>
+    <button
+      className={clsx(
+        "btn",
+        buttonType[type],
+        buttonSize[size],
+        outlined && "btn-outline"
+      )}
+    >
       {icon && !rightIcon && <span className="h-[20px] w-[20px]">{icon}</span>}
       {children}
       {icon && rightIcon && <span className="h-[20px] w-[20px]">{icon}</span>}
